250028003bc0db10947c14de4c33049b97b291c3
Selim Lakehal <selim.lakehal@polymtl.ca>
2023-12-14 23:07:54 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const PORT = 5000;
+app.use('/',(req,res)=>{
+    const payload = "This app is dockerized! 🐳"
+    res.status(200).send(payload);
+})
+
+app.listen(PORT);
+console.log(`Listening to port ${PORT}`)
